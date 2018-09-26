def5315676ed10fe010d8b035a3f153ab410bc10
zhangyu <zhangyu@1124089875@qq.com>
2018-09-26 08:42:17 +0800

    添加token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 //如果是生产环境就用网络的数据库，本地开发就用本地的数据库
 module.exports = {
-    mongoURI:'mongodb://one:lele1111@ds125021.mlab.com:25021/restful-api-product'
+    mongoURI:'mongodb://one:lele1111@ds125021.mlab.com:25021/restful-api-product',
+    secretOrKey:'secret'
 }
--- routers/api/users.js
+++ routers/api/users.js
@@ -7,6 +7,8 @@ const gravatar = require('gravatar');
  
 //引入加密的包给password进行加密
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const secret = require('../../config/keys').secretOrKey
 const User = require('../../models/User')
 
 //在这里写router才会接受到post的数据
@@ -63,6 +65,46 @@ router.post('/register',(req,res) =>{
         //             data: req.body
         //         })
     
+})
+
+//$route  POST api/users/login
+// 返回token jwt passport
+// public
+
+router.post('/login',(req,res) => {
+    const email = req.body.email
+    const password = req.body.password
+//查询数据库，如果user不存在返回状态码404
+    User.findOne({email})
+        .then((user)=>{
+            if(!user){
+                return res.status(404).json({email:'用户不存在'})
+            }
+
+            //密码匹配
+            //第一个是前端发送过来的，第二个是数据库里的，
+            bcrypt.compare(password,user.password)
+                    .then(isMatch => {
+                        if(isMatch){
+                            //我们要返回token，jsonwebtoken即为jwt
+                            jwt.sign('规则','加密名字','过期时间是个对象','箭头函数')
+                            jwt.sign(rule,secret,{expiresIn:3600},(err,token)=>{
+                                if(err){
+                                    throw err
+                                }else{
+                                    res.json({
+                                        success:true,
+                                        token:'zhangyu'+token
+                                    })
+                                }
+                            })
+                            const rule = {id:user.id,name:user.name}
+                            res.json({msg:'success'})
+                        }else{
+                            return res.status(400).json({password:'密码错误'})
+                        }
+                    })
+        })
 })
 
 module.exports = router
